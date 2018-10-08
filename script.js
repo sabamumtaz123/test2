336dd4cc72014947e7084f82fb260cb4dc2f4702
sabamumtaz123 <38943260+sabamumtaz123@users.noreply.github.com>
2018-10-08 17:49:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// write your JS code here
+// let alphabets = ["A","B","C", "D","E","F","G","H","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// function initialize(){
+//     let divEl = document.querySelector(".card-body");
+//     for ( let i=1; i<= 26;i++){
+//         divEl.innerHTML += ` <div class="button">${alphabets[i]}
+      
+//         </div>`
+//     }
+    
+    
+// }
